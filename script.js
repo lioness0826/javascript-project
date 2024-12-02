72a249375e186e9a8ef0f8c1abb3cf9475fb67b1
Lunar248 <lunarfrost24@gmail.com>
2024-12-02 15:11:38 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Point Tracking Code (We can split this up into modules later, if we'd like)
+
+function initializePoints() {
+
+    const defaultPoints = 1000;
+    let playerOnePoints = defaultPoints;
+    let playerTwoPoints = defaultPoints;
+    
+};
+
+
+// Point Tracking Code ends
