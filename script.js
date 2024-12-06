10c11796c0ef0eedb879a166ff28c517814a863b
Eric-024 <horniblowe@gmail.com>
2024-12-05 21:46:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,14 +37,19 @@ $("#reset").addEventListener("mouseup", release);
 These classes set up the deck and the cards. The deck element also has attributes for both the type
 and suit, so it's easier to keep track of which card is which.
 
+
+The deck class will also have the methods for setting up the deck and playing. More details and stuff
+can be found underneath them in another comment.
+
 */
 
 class Card {
 
-    constructor(suit, type){
+    constructor(suit, type, value){
 
         this.suit = suit;
         this.type = type;
+        this.value = value;
 
     }
 
@@ -55,21 +60,27 @@ class Deck {
     constructor() {
         
         this.deck = [];
+        this.handOne = [];
+        this.handTwo = [];
 
         const types = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
         const suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
-        const handOne = [];
-        const handTwo = [];
-        let i = 0
+        const values = [14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+
 
 
         for (let suit in suits) {
 
+            let value = 0
+
             for (let type in types) {
 
-                this.deck.push(new Card(suits[suit], types[type]));
 
-                i++
+                this.deck.push(new Card(suits[suit], types[type], values[value]));
+
+                value += 1
+
 
             }
 
@@ -98,26 +109,155 @@ class Deck {
     
     }
 
+    getHandOne () {
+
+        while (this.handOne.length < 3) {
+    
+            this.handOne.push(this.deck.pop())
+    
+        }
+
+        this.handOne.sort((x, y) => x.value - y.value);
+
+        return this.handOne
+
+    }
+
+    getHandTwo () {
+
+        while (this.handTwo.length < 3) {
+    
+            this.handTwo.push(this.deck.pop())
+            
+        }
+        
+        this.handTwo.sort((x, y) => x.value - y.value);
+
+        return this.handTwo
+        
+    }
+
+    emptyHand () {
+
+        while (this.handOne.length > 0) {
+
+            this.deck.push(this.handOne.pop())
+
+        }
+
+        
+        while (this.handTwo.length > 0) {
+
+            this.deck.push(this.handTwo.pop())
+            
+        }
+    }
+
+    /* 
+
+    For three card poker, the best hands from top to bottom are:
+    
+    Straight flush – three consecutive cards of the same suit
+    Three-of-a-kind – three cards of the same rank
+    Straight – three consecutive cards of mixed suits
+    Flush – three cards of the same suit
+    Pair – two cards of the same rank
+    High card
+
+    */
+
+    getHandScores () {
+
+        let fstValOne = this.handOne[0].value
+        let fstValTwo = this.handOne[1].value
+        let fstValThree = this.handOne[2].value
+
+        let sndValOne = this.handTwo[0].value
+        let sndValTwo = this.handTwo[1].value
+        let sndValThree = this.handTwo[2].value
+
+        //This is just to make writing the elif statments shorter and easier.
+        
+        //Straight flush
+        //Three-of-a-kind
+        //Straight
+        //Flush
+
+        if(this.handOne[0].suit == this.handOne[1].suit & this.handOne[1].suit == this.handOne[2].suit) {
+            console.log("Flush, Player 1")
+        }
+
+        
+        if(this.handTwo[0].suit == this.handTwo[1].suit & this.handTwo[1].suit == this.handTwo[2].suit) {
+            console.log("Flush, Player 2")
+        }
+        //Pair      
+        
+        if(fstValOne === fstValTwo || fstValTwo === fstValThree || fstValOne === fstValThree) {
+            console.log("Pair, Player 1")
+        }
+        if (sndValOne === sndValTwo || sndValTwo === sndValThree || sndValOne === sndValThree) {
+            console.log("Pair, Player 2")
+        }
+
+        //High card
+        
+        for (let i = 2; i >= 0;) {
+
+            if(this.handOne[i].value > this.handTwo[i].value) {
+                return console.log("High Card, Player 1")
+
+            } else if(this.handOne[i].value < this.handTwo[i].value) {
+                return console.log("High Card, Player 2")
+
+            }
+            
+            i--
+
+        }
+
+        /*
+        if (fstValThree > sndValThree) {
+            console.log("Player One Wins")
+        } else if (fstValThree < sndValThree) {
+            console.log("Player Two Wins") 
+        } else if (fstValTwo > sndValTwo) {
+            console.log("Player One Wins")
+        } else if (fstValTwo < sndValTwo) {
+            console.log("Player Two Wins")
+        } else if (fstValOne > sndValTwo) {
+            console.log("Player One Wins")
+        } else if (fstValOne < sndValTwo) {
+            console.log("Player Two Wins")
+        } else {console.log("Draw")}
+        */
+    }
+
 }                
 
 const playingDeck = new Deck();
-playingDeck.getShuffledDeck()
+/* playingDeck.getShuffledDeck()
+playingDeck.getHandOne()
+playingDeck.getHandTwo()
+playingDeck.getHandScores() */
 
-/* 
+for (let i = 1; i < 100; i++) {
+    console.log(i)
+    playingDeck.getShuffledDeck()
+    playingDeck.getHandOne()
+    playingDeck.getHandTwo()
+    playingDeck.getHandScores()
+    playingDeck.emptyHand()
+}
 
-The shuffling function was added to the Deck class as a method, if you need to shuffle the deck, you'd just do playingDeck.getShuffledDeck
+/*  Below here is a list of methods you can use to set up the deck.
 
-Otherwise if you try to pop a card, it'll always pop the same card from the end of the deck.
+    
+playingDeck.getShuffledDeck() - Shuffles the Deck (Do this every time before you deal the cards)
+playingDeck.getHandOne() - Gets player one's hand
+playingDeck.getHandTwo() - Gets player two's hand
+playingDeck.emptyHand() - Empties the hands of both players and adds them back to the deck.
 
-The shuffle algorithm will shuffle the 'playingDeck.deck' array.
-
-*/
-
-// Point Tracking Code
-
-/*
-
-This will be for tracking the actual scoring of points, betting, and similar.
 
 */
 
@@ -144,15 +284,6 @@ let betTwo = {
     playerOne: 0,
     playerTwo: 0
     
-    }
-
-// Hand Class Definition
-
-let hand = {
-
-        playerOneHand: [],
-        playerTwoHand: []
-
     }
 
 
@@ -188,27 +319,7 @@ function secondForfeit(playerNum, bet) {
 
 
 
-function getHand () {
 
-
-
-
-
-    while (hand.playerOneHand.length < 3) {
-
-        hand.playerOneHand.push(playingDeck.deck.pop())
-
-    }
-
-    while (hand.playerTwoHand.length < 3) {
-
-        hand.playerTwoHand.push(playingDeck.deck.pop())
-        
-    }
-
-    return hand;
-    
-}
 
 
 function getScore (hand) {
